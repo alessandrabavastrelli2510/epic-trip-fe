871088b03edb840ac70d1a48259eafb73104e3a5
Francesco Aversali <165185691+Gunsxdd@users.noreply.github.com>
2024-06-19 00:52:07 +0200

    inizia il miglioramento della grafica della home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HolidayPackageListComponent } from '../holiday-package-list/holiday-package-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule,HolidayPackageListComponent],
+  imports: [RouterModule,HolidayPackageListComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  
+  currentSlide = 0;
+  totalSlides = 3;
+
+  ngOnInit() {
+    this.initCarousel();
+  }
+
+  initCarousel() {
+    setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+    }, 5000);
+  }
+
 }
