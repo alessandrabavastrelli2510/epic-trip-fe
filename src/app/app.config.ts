4d19f55f4e4b8a828b451222f166236887d4945d
ranenrico <ran.enrico00@gmail.com>
2024-06-19 23:12:46 +0200

    aggiorna app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
-import { BrowserModule } from '@angular/platform-browser';
-import { MapComponent } from './map/map.component';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +12,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    importProvidersFrom(BrowserModule, MapComponent)
 ]
 };
